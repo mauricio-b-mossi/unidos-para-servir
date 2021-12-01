7664b24694d6090b0fe202d326b6c95c7dacbf3f
Mauricio Mossi <mauricio.b.mossi@gmail.com>
2021-11-30 22:05:15 -0500

    switched fundadores layout

--- components/NuestrosFundadores.js
+++ components/NuestrosFundadores.js
@@ -9,7 +9,7 @@ export default function NuestrosFundadores({ nuestrosFundadores, members }) {
   
   return (
     <div id='fundadores' className="flex justify-center items-center h-auto  w-full p-4 sm:p-6 md:p-8 lg:p-10 ">
-      <div className="flex flex-col justify-center items-center min-h-screen max-w-7xl space-y-10 lg:space-y-20 border-t-2">
+      <div className="flex flex-col justify-center items-center max-w-7xl space-y-10 lg:space-y-20 border-t-2">
         {/* Title */}
         <div className="flex flex-col justify-center items-center space-y-4">
           <div className="flex-col text-3xl lg:text-5xl font-serif font-semibold leading-snug pt-12">
